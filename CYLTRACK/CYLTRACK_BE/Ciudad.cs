1bcaedeb9382e63ddedaf1e29781d9677cd9fffe
vivis1225 <vivis1225@992608a5-e751-9426-943c-320f8e000771>
2013-01-13 21:55:24 +0000

    Viviana Camacho - 8 primeras Clases Entidades del Negocio 13/01/13

---
+++ CYLTRACK/CYLTRACK_BE/Ciudad.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Proyecto de grado: Trazabilidad de Cilindros CYLTRACK
+ * Integrantes: Viviana Camacho y Jackelyne Padilla
+ * Director: Fabián Lancheros Currea
+ * Deerechos reservados
+ * */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CYLTRACK_BE
+{
+    /// <summary>
+    /// Clase utilizada para representar la entidad Ciudad
+    /// </summary>
+    class Ciudad
+    {
+        /// <summary>
+        /// Identificador de la ciudad
+        /// </summary>
+        public String Id_Ciudad { get; set; }
+        
+        /// <summary>
+        /// Nombre de la ciudad
+        /// </summary>
+        public String Nombre { get; set; }
+        
+    }
+}
---
+++ CYLTRACK/CYLTRACK_BE/Conductor_Vehiculo.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Proyecto de grado: Trazabilidad de Cilindros CYLTRACK
+ * Integrantes: Viviana Camacho y Jackelyne Padilla
+ * Director: Fabián Lancheros Currea
+ * Deerechos reservados
+ * */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CYLTRACK_BE
+{
+    /// <summary>
+    /// Clase utilizada para representar la entidad Conductor_Vehículo
+    /// </summary>
+    
+    class Conductor_Vehiculo
+    {
+        /// <summary>
+        /// Identificador de conductor_vehículo
+        /// </summary>
+        public String Id_Conductor_Vehiculo { get; set; }
+
+        /// <summary>
+        /// fecha de inicio de la ruta en un vehículo
+        /// </summary>
+        public DateTime  Fecha_Inicio { get; set; }
+
+        /// <summary>
+        /// fecha de finalización de la ruta en un vehículo
+        /// </summary>
+        public DateTime Fecha_Fin { get; set; }
+
+    }
+}
