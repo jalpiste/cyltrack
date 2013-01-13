8ff6021d3500ff221702a8cd134f6d37e95ed7ee
vivis1225 <vivis1225@992608a5-e751-9426-943c-320f8e000771>
2013-01-13 20:14:00 +0000

    Viviana Camacho - Creación de clases en el proyecto de Entidades de Negocio 13/01/13

---
+++ trunk/CYLTRACK/CYLTRACK_BE/Cilindro.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * Proyecto de grado: Trazabilidad de Cilindros CYLTRACK
+ * Integrantes: Viviana Camacho y Jackelyne Padilla
+ * Director: Fabián Lancheros Currea
+ * Deerechos reservados
+ * */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CYLTRACK_BE
+{
+    /// <summary>
+    /// Clase utilizada para representar la entidad Cilindro
+    /// </summary>
+    class Cilindro
+    {
+        /// <summary>
+        /// Identificador del cilindro
+        /// </summary>
+        public String Id_Cilindro { get; set; }
+        /// <summary>
+        /// Año de fabricación del cilindro
+        /// </summary>
+        public String Ano { get; set; }
+        /// <summary>
+        /// Identificador fabricante de cilindro, 3 o 4 digitos
+        /// </summary>
+        public String Id_Fabricante { get; set; }
+        /// <summary>
+        /// Código de 4 digitos restantes
+        /// </summary>
+        public String Codigo_Cilindro { get; set; }
+        /// <summary>
+        /// Tamaño del cilindro
+        /// </summary>
+        public String Tamano { get; set; }
+        /// <summary>
+        /// Estado actual del cilindro, en uso o chatarra
+        /// </summary>
+        public String Estado { get; set; }
+        /// <summary>
+        /// Tipo de cilindro, universal o de marca
+        /// </summary>
+        public String Tipo_Cilindro { get; set; }
+    }
+}
