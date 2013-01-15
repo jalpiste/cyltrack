a5ac8bc7a119f329088561e6da3338b623c3d9bf
cesarcurrea <cesarcurrea@992608a5-e751-9426-943c-320f8e000771>
2013-01-15 05:09:28 +0000

    Ajustes Fabian Lancheros

--- trunk/CYLTRACK/CYLTRACK_WCF_Services/Service1.cs
+++ trunk/CYLTRACK/CYLTRACK_WCF_Services/Service1.cs
@@ -10,22 +10,9 @@ namespace Unisangil.CYLTRACK.WCF_Services
     // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código y en el archivo de configuración a la vez.
     public class Service1 : IService1
     {
-        public string GetData(int value)
+        public string Prueba(int value)
         {
-            return string.Format("You entered: {0}", value);
-        }
-
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
-        {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
+            return "Hola, prueba";
         }
     }
 }
