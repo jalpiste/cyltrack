f11828e93cdeb870f58c9f97a149804f4a6c9fd7
cesarcurrea <cesarcurrea@992608a5-e751-9426-943c-320f8e000771>
2013-04-15 02:31:06 +0000

    Correcciones Fabian - 14 Abril 2013

--- CYLTRACK/CYLTRACK_Test/AppTest/Program.cs
+++ CYLTRACK/CYLTRACK_Test/AppTest/Program.cs
@@ -2,13 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Unisangil.CYLTRACK.CYLTRACK_BE;
+using Unisangil.CYLTRACK.AppTest;
+using AppTest.PruebaService;
 
-namespace AppTest
+namespace Unisangil.CYLTRACK.AppTest
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            Program pp = new Program();
+            long resp = pp.CrearPrueba();
+            Console.WriteLine("Codigo creado: " + resp);
+            Console.Read();
+            
+        }
+
+        public long CrearPrueba()
+        {
+            Cilindro cilindro = new Cilindro();
+            cilindro.Tamano = "40";
+            cilindro.Tipo_Cilindro = "GPL";
+            cilindro.Id_Fabricante = "123455556";
+            long resp = 0;
+            try{
+                Service1Client serv = new Service1Client();
+                resp = serv.Prueba(cilindro);
+            }
+            catch
+            {
+                resp = -1;
+            }
+            return resp;
         }
     }
 }
