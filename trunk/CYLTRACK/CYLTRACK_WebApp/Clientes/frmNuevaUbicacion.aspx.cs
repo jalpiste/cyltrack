ad6c9278143cb4d1869c35ab9d0aab39a1721798
vivis1225 <vivis1225@992608a5-e751-9426-943c-320f8e000771>
2013-04-17 04:26:35 +0000

    Unisangil.CYLTRACK. para las interfaces

--- trunk/CYLTRACK/CYLTRACK_WebApp/Clientes/frmNuevaUbicacion.aspx.cs
+++ trunk/CYLTRACK/CYLTRACK_WebApp/Clientes/frmNuevaUbicacion.aspx.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace CYLTRACK_WebApp.Clientes
+namespace Unisangil.CYLTRACK.CYLTRACK_WebApp.Clientes
 {
     public partial class frmNuevaUbicacion : System.Web.UI.Page
     {
