a67678d2020c405a6f8aa15ddcb870ed4ee36be9
vivis1225 <vivis1225@992608a5-e751-9426-943c-320f8e000771>
2013-05-11 09:34:47 +0000

    modificacion clases pedido y vehiculoBL

--- CYLTRACK/CYLTRACK_BL/PedidoBL.cs
+++ CYLTRACK/CYLTRACK_BL/PedidoBL.cs
@@ -18,24 +18,66 @@ namespace Unisangil.CYLTRACK.CYLTRACK_BL
 
         #endregion
         #region Metodos publicos
-        public long RegistrarPedido(PedidoBE registrar_ped)
+        /// <summary>
+        /// Método para el registro de pedidos en el sistema
+        /// </summary>
+        /// <param name="registrar_ped"></param>
+        /// <returns></returns>
+        public String RegistrarPedido(PedidoBE registrar_ped)
         {
-            return 0;
+            String resp = "1";
+            return resp;
         }
-
-        public long ConsultarPedido(PedidoBE consultar_ped)
+        /// <summary>
+        /// Método para la consulta de pedidos en el sistema
+        /// </summary>
+        /// <param name="consultar_ped"></param>
+        /// <returns></returns>
+        public List<PedidoBE> ConsultarPedido(PedidoBE consultar_ped)
         {
-            return 0;
+            List<PedidoBE> lstPedido = new List<PedidoBE>();
+            PedidoBE pedido = new PedidoBE();
+            pedido.Cliente.Cedula = "1053544325";
+            pedido.Cliente.Nombres_Cliente = "María Clara";
+            pedido.Cliente.Apellido_1 = "Guzmán";
+            pedido.Cliente.Apellido_2 = "Díaz";
+            pedido.Ubicacion.Direccion = "Calle 13 N 4-47";
+            pedido.Ubicacion.Barrio = "Las Ferias";
+            pedido.Ciudad.Nombre_Ciudad = "Chiquinquirá";
+            pedido.Ciudad.Departamento.Nombre_Departamento = "Boyacá";
+            pedido.Ubicacion.Telefono_1 = "3214768823";
+            //--------------------------------
+            pedido.Vehiculo.Placa = "XHA768";
+            pedido.Ruta.Nombre_Ruta = "Chiquinquirá-Ubaté";
+            pedido.Detalle_Ped.Tamano.Tamano  = "30";
+            pedido.Detalle_Ped.Cantidad = "9";
+            pedido.Fecha  = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            pedido.Venta.Fecha = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+
+            lstPedido.Add(pedido);
+            return lstPedido;
         }
-
-        public long ModificarPedido(PedidoBE modificar_ped)
+        /// <summary>
+        /// Método para la modificación de pedidos en el sistema
+        /// </summary>
+        /// <param name="modificar_ped"></param>
+        /// <returns></returns>
+        public String ModificarPedido(PedidoBE modificar_ped)
         {
-            return 0;
+            List<PedidoBE> consulta = ConsultarPedido(modificar_ped);
+            String resp = "Pedido Modificado";
+            return resp;
         }
-
-        public long CancelarPedido(PedidoBE cancelar_ped)
+        /// <summary>
+        /// Método para la cancelación de pedidos en el sistema
+        /// </summary>
+        /// <param name="cancelar_ped"></param>
+        /// <returns></returns>
+        public String CancelarPedido(PedidoBE cancelar_ped)
         {
-            return 0;
+            List<PedidoBE> consulta = ConsultarPedido(cancelar_ped);
+            String resp = "Pedido Cancelado";
+            return resp;
         }
         #endregion
         #region Metodos privados
