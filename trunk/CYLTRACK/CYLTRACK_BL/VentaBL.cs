6e56d4c4e0fd0067eeef8d4f087d8fcc7c8dffe4
jackpave0917 <jackpave0917@992608a5-e751-9426-943c-320f8e000771>
2013-05-11 04:28:18 +0000

    modificacion clase ventaBL capa de negocio

--- trunk/CYLTRACK/CYLTRACK_BL/VentaBL.cs
+++ trunk/CYLTRACK/CYLTRACK_BL/VentaBL.cs
@@ -18,24 +18,56 @@ namespace Unisangil.CYLTRACK.CYLTRACK_BL
 
         #endregion
         #region Metodos publicos
-        public long VentaCilindro(VentaBE ventas)
+        public String VentaCilindro(VentaBE ventas)
         {
-            return 0;
+            String resp = "Venta registrada";
+            return resp;
         }
 
-        public long ConsultarVenta(VentaBE ventas)
+        public List<VentaBE> ConsultarVenta(VentaBE ventas)
         {
-            return 0;
+            List<VentaBE> lstVenta = new List<VentaBE>();
+            VentaBE venta = new VentaBE();
+            venta.Fecha = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            venta.Cliente.Cedula = "7302834";
+            venta.Cliente.Nombres_Cliente = "Pablo";
+            venta.Cliente.Apellido_1 = "Paez";
+            venta.Cliente.Apellido_2 = "Veloza";
+            venta.Ubicacion.Direccion = "Cra 9 N° 8 34";
+            venta.Ubicacion.Barrio = "Boyacá";
+            venta.Ubicacion.Ciudad.Departamento.Nombre_Departamento = "Boyacá";
+            venta.Ubicacion.Ciudad.Nombre_Ciudad = "Chiquinquirá";
+            venta.Ubicacion.Telefono_1 = "7266617";
+            venta.Cilindro.Codigo_Cilindro = "67865675678";
+            venta.Cilindro.NTamano.Tamano = "40";
+            venta.Observaciones = "error en el codigo del cilindro del cliente";
+            //------------------------
+            venta.Vehiculo.Conductor_Vehiculo.Conductor.Nombres_Conductor = "carlos";
+            venta.Vehiculo.Conductor_Vehiculo.Conductor.Apellido_1 = "Pineda";
+            venta.Vehiculo.Conductor_Vehiculo.Conductor.Apellido_2 = "Poveda";
+            venta.Vehiculo.Placa = "XHA940";
+            venta.Vehiculo.Ruta.Nombre_Ruta = "Zona centro";
+
+            lstVenta.Add(venta);
+            return lstVenta;
         }
 
-        public long RevisionCasosEspeciales(VentaBE ventas)
+        public List<CasosBE> RevisionCasosEspeciales(CasosBE casos)
         {
-            return 0;
+            List<CasosBE> lstCaso = new List<CasosBE>();
+            CasosBE caso = new CasosBE();
+            caso.Id_Casos="98989";
+            caso.Tipo_Caso.Nombre_Caso = "Escape";
+            lstCaso.Add(caso);
+
+            return lstCaso;
         }
 
-        public long CasosEspeciales(VentaBE ventas)
+        public String CasosEspeciales(VentaBE ventas)
         {
-            return 0;
+            List<VentaBE> consulta = ConsultarVenta(ventas);
+            String resp = "caso especial registrado";
+            return resp;
         }
         #endregion
         #region Metodos privados
