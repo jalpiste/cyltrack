eeb33c3df05628098b8854d7744fb709317e68c7
jackpave0917 <jackpave0917@992608a5-e751-9426-943c-320f8e000771>
2013-05-11 04:25:48 +0000

    modificacion clase cilindroBL capa de negocio

--- CYLTRACK/CYLTRACK_BL/CilindroBL.cs
+++ CYLTRACK/CYLTRACK_BL/CilindroBL.cs
@@ -10,6 +10,8 @@ using System.Linq;
 using System.Text;
 using Unisangil.CYLTRACK.CYLTRACK_BE;
 
+
+
 namespace Unisangil.CYLTRACK.CYLTRACK_BL
 {
     public class CilindroBL
@@ -23,24 +25,57 @@ namespace Unisangil.CYLTRACK.CYLTRACK_BL
         /// </summary>
         /// <param name="cilindro"></param>
         /// <returns></returns>
-        public long CrearCilindro(CilindroBE cilindro)
+        public String CrearCilindro(CilindroBE cilindro)
         {
-            return 0;
+            String resp = "1";
+            return resp;
         }
         
-        public long ConsultarCilindro(CilindroBE cilindro)
+        public List<CilindroBE> ConsultarCilindro(CilindroBE cilindro)
         {
-            return 0;
+            List<CilindroBE> lstCil = new List<CilindroBE>();
+            CilindroBE cil = new CilindroBE();
+            cil.Ano = "2012";
+            cil.Fabricante.Nombre_Fabricante = "Cilgas";
+            cil.Serial_Cilindro = "51954";
+            cil.Ubicacion.Tipo_Ubicacion.Nombre_Ubicacion = "Plataforma";
+            cil.NTamano.Tamano = "30";
+            cil.Codigo_Cilindro = "12091751964";
+            cil.Fecha = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            //--------------------------------
+            cil.Venta.Cliente.Cedula = "7309877";
+            cil.Venta.Cliente.Nombres_Cliente = "Jaime Andres";
+            cil.Venta.Cliente.Apellido_1 = "Ortiz";
+            cil.Venta.Cliente.Apellido_2 = "León";
+            cil.Venta.Cliente.Ubicacion.Direccion = "Calle 3 N 2-49";
+            cil.Venta.Cliente.Ubicacion.Barrio = "El Bosque";
+            cil.Venta.Cliente.Ciudad.Nombre_Ciudad = "Chiquinquirá";
+            cil.Venta.Cliente.Ciudad.Departamento.Nombre_Departamento = "Boyacá";
+            cil.Venta.Cliente.Ubicacion.Telefono_1 = "7266530";
+            cil.Venta.Fecha = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            //-----------------------------
+            cil.Vehiculo.Placa = "XHA940";
+            cil.Vehiculo.Conductor_Vehiculo.Conductor.Nombres_Conductor = "Juanito perez";
+            cil.Vehiculo.Ruta.Nombre_Ruta = "Chiquinquirá-Ubaté";
+            
+            lstCil.Add(cil);
+            return lstCil;
         }
 
-        public long AsignarUbicacion(CilindroBE cilindro)
+        public String AsignarUbicacion(CilindroBE cilindro)
         {
-            return 0;
+            return "Ok";
         }
 
-        public long CargueyDescargueCilindro(CilindroBE cilindro)
+        public List<CilindroBE> CargueyDescargueCilindro(CilindroBE cilindro)
         {
-            return 0;
+            List<CilindroBE> lstCod = new List<CilindroBE>();
+            CilindroBE cil = new CilindroBE();
+            //cargue de cilindros
+            cil.Codigo_Cilindro = "87867675675";
+            cil.Codigo_Cilindro = "74875738658";
+
+            return lstCod;
         }
         #endregion
         #region Metodos privados
