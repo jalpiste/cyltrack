7f83a9b6d312e4cdb73aac610c63e4f8770c51dc
jackpave0917 <jackpave0917@992608a5-e751-9426-943c-320f8e000771>
2013-05-28 05:45:09 +0000

    Modificacion cyltrack_phone

--- CYLTRACK/CYLTRACK_PHONE/Clientes/frmRegistrarCliente.xaml.cs
+++ CYLTRACK/CYLTRACK_PHONE/Clientes/frmRegistrarCliente.xaml.cs
@@ -11,7 +11,7 @@ using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
 
-namespace Cyltrack_phone.Clientes
+namespace Unisangil.CYLTRACK.Cyltrack_phone.Clientes
 {
     public partial class frmRegistrarCliente : PhoneApplicationPage
     {
