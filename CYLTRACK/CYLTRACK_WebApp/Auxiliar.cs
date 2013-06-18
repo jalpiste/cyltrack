c5c390e9487483bf8536407ef261e7980e52d9bc
jackpave0917 <jackpave0917@992608a5-e751-9426-943c-320f8e000771>
2013-06-18 04:03:26 +0000

    creacion clase auxiliar interfaces web

---
+++ CYLTRACK/CYLTRACK_WebApp/Auxiliar.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Unisangil.CYLTRACK.CYLTRACK_WebApp
+{
+    public static class Auxiliar
+    {
+
+        public static List<string> ConsultarMeses()
+        {
+            List<string> meses = new List<string>();
+            meses = Enum.GetNames(typeof(Meses)).ToList();
+            return meses;
+        }
+
+        public static Dias[] ConsultarDias()
+        {
+            Dias[] dias = new Dias[32];
+            for (int i = 1; i < 32; i++) 
+            {
+                dias[i]+=i;
+            }
+                return dias;
+        }
+
+        public static Anos[] ConsultarAnos()
+        {
+            int aux=(DateTime.Now.Year)-91;
+            Anos[] Ano = new Anos[77];
+            for (int i = 1; i < 77; i++)
+            {
+                Ano[i]+= aux+i;
+            }
+            return Ano;
+        }
+
+        public static List<string> ConsultarSexo()
+        {
+            List<string> sexo = new List<string>();
+            sexo = Enum.GetNames(typeof(Sexo)).ToList();
+            return sexo;
+        }
+
+      }
+
+    public enum Meses
+    {
+        Enero = 1,
+        Febrero = 2,
+        Marzo = 3,
+        Abril = 4,
+        Mayo = 5,
+        Junio = 6,
+        Julio = 7,
+        Agosto = 8,
+        Septiembre = 9,
+        Octubre = 10,
+        Noviembre = 11,
+        Diciembre = 12
+    }
+    public enum Dias
+    {
+        Dia
+    }
+
+    public enum Anos
+    {
+        Ano
+    }
+
+    public enum Sexo
+    {
+        Mujer = 1,
+        Hombre = 2,
+        Indefinido = 3
+    }
+
+   
+   
+}
