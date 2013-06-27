29c8ef429f76bd20e7694fd2928d5bf44c555848
vivis1225 <vivis1225@992608a5-e751-9426-943c-320f8e000771>
2013-06-27 22:39:46 +0000

    modificaciones capa de negocio interfaces Cliente, Vehiculo y pedido

--- CYLTRACK/CYLTRACK_BL/PedidoBL.cs
+++ CYLTRACK/CYLTRACK_BL/PedidoBL.cs
@@ -25,7 +25,7 @@ namespace Unisangil.CYLTRACK.CYLTRACK_BL
         /// <returns></returns>
         public String RegistrarPedido(PedidoBE registrar_ped)
         {
-            String resp = "1";
+            String resp = "Ok";
             return resp;
         }
         /// <summary>
@@ -37,22 +37,40 @@ namespace Unisangil.CYLTRACK.CYLTRACK_BL
         {
             List<PedidoBE> lstPedido = new List<PedidoBE>();
             PedidoBE pedido = new PedidoBE();
-            pedido.Cliente.Cedula = "1053544325";
-            pedido.Cliente.Nombres_Cliente = "María Clara";
-            pedido.Cliente.Apellido_1 = "Guzmán";
-            pedido.Cliente.Apellido_2 = "Díaz";
-            pedido.Ubicacion.Direccion = "Calle 13 N 4-47";
-            pedido.Ubicacion.Barrio = "Las Ferias";
-            pedido.Ciudad.Nombre_Ciudad = "Chiquinquirá";
-            pedido.Ciudad.Departamento.Nombre_Departamento = "Boyacá";
-            pedido.Ubicacion.Telefono_1 = "3214768823";
-            //--------------------------------
-            pedido.Vehiculo.Placa = "XHA768";
-            pedido.Ruta.Nombre_Ruta = "Chiquinquirá-Ubaté";
-            pedido.Detalle_Ped.Tamano.Tamano  = "30";
-            pedido.Detalle_Ped.Cantidad = "9";
-            pedido.Fecha  = Convert.ToDateTime(DateTime.Now.ToShortDateString());
-            pedido.Venta.Fecha = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+
+            pedido.Id_Pedido = "45532";
+
+            ClienteBE ped = new ClienteBE();
+            ped.Cedula = "33134665";
+            ped.Nombres_Cliente = "María Clara";
+            ped.Apellido_1 = "Guzmán";
+            ped.Apellido_2 = "Díaz";
+            pedido.Cliente = ped;
+
+            UbicacionBE ubi_cli = new UbicacionBE();
+            ubi_cli.Direccion = "Calle 13 N 4-47";
+            ubi_cli.Barrio = "Las Ferias";
+            ubi_cli.Telefono_1 = "3214768823";
+            pedido.Ubicacion = ubi_cli;
+
+            CiudadBE ciu_cli = new CiudadBE();
+            ciu_cli.Nombre_Ciudad = "Chiquinquirá";
+            pedido.Ciudad = ciu_cli;
+
+            DepartamentoBE dep_cli = new DepartamentoBE();
+            dep_cli.Nombre_Departamento = "Boyacá";
+            ciu_cli.Departamento = dep_cli;
+
+            ////--------------------------------
+            VehiculoBE veh_ped = new VehiculoBE();
+            veh_ped.Placa = "XHA768";
+            pedido.Vehiculo = veh_ped;
+
+            RutaBE ruta_ped = new RutaBE();
+            ruta_ped.Nombre_Ruta = "Chiquinquirá-Ubaté";
+            pedido.Ruta = ruta_ped;
+
+            pedido.Fecha = Convert.ToDateTime(DateTime.Now.ToShortDateString());
 
             lstPedido.Add(pedido);
             return lstPedido;
@@ -65,7 +83,7 @@ namespace Unisangil.CYLTRACK.CYLTRACK_BL
         public String ModificarPedido(PedidoBE modificar_ped)
         {
             List<PedidoBE> consulta = ConsultarPedido(modificar_ped);
-            String resp = "Pedido Modificado";
+            String resp = "Ok";
             return resp;
         }
         /// <summary>
@@ -76,7 +94,7 @@ namespace Unisangil.CYLTRACK.CYLTRACK_BL
         public String CancelarPedido(PedidoBE cancelar_ped)
         {
             List<PedidoBE> consulta = ConsultarPedido(cancelar_ped);
-            String resp = "Pedido Cancelado";
+            String resp = "Ok";
             return resp;
         }
         #endregion
