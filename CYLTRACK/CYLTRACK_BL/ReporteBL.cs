95075401e89186e25568b1473fb7db7046b09c55
jackpave0917 <jackpave0917@992608a5-e751-9426-943c-320f8e000771>
2013-07-20 22:21:30 +0000

    modificacion CilindroBL, reporteBL, RutaBL

--- CYLTRACK/CYLTRACK_BL/ReporteBL.cs
+++ CYLTRACK/CYLTRACK_BL/ReporteBL.cs
@@ -43,32 +43,45 @@ namespace Unisangil.CYLTRACK.CYLTRACK_BL
         public List<ReportesBE> Inventario(ReportesBE reporte)
         {
             List<ReportesBE> resp= new List<ReportesBE>();
-            ReportesBE report = new ReportesBE();
+            ReportesBE[] lista = new ReportesBE[7];
+            Random ran = new Random();
+            for (int i = 0; i < 7; i++)
+            {
+                ReportesBE datReporte = new ReportesBE();
 
-            Tipo_UbicacionBE tipoUbica = new Tipo_UbicacionBE();
-            UbicacionBE ubi = new UbicacionBE();
-            ubi.Tipo_Ubicacion = tipoUbica;
-            report.Ubicacion = ubi;
-            VehiculoBE veh = new VehiculoBE();
-            report.Vehiculo = veh;
-            CilindroBE cil = new CilindroBE();
-            report.Cilindro = cil;
-            if (reporte.Ubicacion.Tipo_Ubicacion.Nombre_Ubicacion == "Vehiculo")
-            {
-                report.Ubicacion.Tipo_Ubicacion.Nombre_Ubicacion = "XHA940";
-                report.Cilindro.Tipo_Cilindro = "Universal";
-                report.Cilindro.Cantidad = 5;
-            } 
-            else 
-            {
-                report.Ubicacion.Tipo_Ubicacion.Nombre_Ubicacion = reporte.Ubicacion.Tipo_Ubicacion.Nombre_Ubicacion;
-                report.Cilindro.Tipo_Cilindro = reporte.Cilindro.Tipo_Cilindro;
-                report.Cilindro.Cantidad = 5;
+                CilindroBE cil = new CilindroBE();
+                cil.Codigo_Cilindro = ((DateTime.Now.Hour + DateTime.Now.Second) * ran.Next(1, 10)).ToString();
+                cil.Tipo_Cilindro = "Marcado";
+                TamanoBE tam = new TamanoBE();
+                tam.Tamano = "30";
+                cil.NTamano = tam;
+                datReporte.Cilindro = cil;
+                VehiculoBE veh = new VehiculoBE();
+                veh.Placa = "UIZ987";
+                UbicacionBE ubicacion = new UbicacionBE();
+                Tipo_UbicacionBE tipoUbica = new Tipo_UbicacionBE();
+                ubicacion.Tipo_Ubicacion = tipoUbica;
+                ubicacion.Vehiculo = veh;
+                datReporte.Ubicacion = ubicacion;
+
+                if (reporte.Ubicacion.Tipo_Ubicacion.Nombre_Ubicacion == "Vehiculo")
+                {
+                    datReporte.Ubicacion.Tipo_Ubicacion.Nombre_Ubicacion = "XHA940";
+                    datReporte.Cilindro.Tipo_Cilindro = "Universal";
+                    datReporte.Cilindro.Cantidad = 5;
+                }
+                else
+                {
+                    datReporte.Ubicacion.Tipo_Ubicacion.Nombre_Ubicacion = reporte.Ubicacion.Tipo_Ubicacion.Nombre_Ubicacion;
+                    datReporte.Cilindro.Tipo_Cilindro = reporte.Cilindro.Tipo_Cilindro;
+                    datReporte.Cilindro.Cantidad = 5;
+                }
+            
+                lista[i] = datReporte;
             }
-            
-            
-            
-            resp.Add(report);
+
+            resp = lista.ToList();
+
             return resp;
         }
 
