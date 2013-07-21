3302f1ec82d912f26a2365676616c50ab15d71f4
vivis1225 <vivis1225@992608a5-e751-9426-943c-320f8e000771>
2013-07-21 03:15:56 +0000

    modificacion interfaces pedido

--- trunk/CYLTRACK/CYLTRACK_PHONE/Pedidos/frmRegistrarPedido.xaml.cs
+++ trunk/CYLTRACK/CYLTRACK_PHONE/Pedidos/frmRegistrarPedido.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using CYLTRACK_PHONE.PedidoService;
 
 namespace Unisangil.CYLTRACK.Cyltrack_phone.Pedidos
 {
@@ -22,8 +23,38 @@ namespace Unisangil.CYLTRACK.Cyltrack_phone.Pedidos
 
         private void btnRegistrar_Click(object sender, RoutedEventArgs e)
         {
-            ContentBusq.Visibility = System.Windows.Visibility.Collapsed;
-            ContentRegistrar.Visibility = System.Windows.Visibility.Visible;
+
+            PedidoServiceClient servPedido = new PedidoServiceClient();
+            PedidoBE pedido = new PedidoBE();
+            //PedidoBE[] consulta = servPedido.Consultar_PedidoAsync(pedido);
+            //PedidoBE consultar_ped = new PedidoBE();
+
+            try
+            {
+
+                //foreach (PedidoBE info in consulta)
+                //{
+                //    txtNombres.Text = info.Cliente.Nombres_Cliente;
+                //    txtPrApellido.Text = info.Cliente.Apellido_1;
+                //    txtSgApellido.Text = info.Cliente.Apellido_2;
+                //    lstDir.SelectedValue = info.Ubicacion.Direccion;// como llamar todas las direcciones disponibles para el cliente???
+                //    lblCiudad.Text = info.Ciudad.Nombre_Ciudad;
+                //    lstTel.SelectedValue = info.Ubicacion.Telefono_1;
+                //}
+
+                ContentBusq.Visibility = System.Windows.Visibility.Collapsed;
+                ContentRegistrar.Visibility = System.Windows.Visibility.Visible;
+            }
+            catch (Exception ex)
+            {
+                NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+            }
+            finally
+            {
+                servPedido.CloseAsync();
+                NavigationService.Navigate(new Uri("/Pedidos/frmRegistrarPedido.xaml", UriKind.Relative));
+            }
+ 
         }
 
         private void btnMenu_Click(object sender, RoutedEventArgs e)
@@ -31,12 +62,46 @@ namespace Unisangil.CYLTRACK.Cyltrack_phone.Pedidos
             NavigationService.GoBack();
         }
 
-     
+
         private void btnGuardar_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Sus datos fueron enviados satisfactoriamente");
-            ContentRegistrar.Visibility = System.Windows.Visibility.Collapsed;
-            ContentBusq.Visibility = System.Windows.Visibility.Visible;
-        }   
+            PedidoServiceClient servPedido = new PedidoServiceClient();
+            PedidoBE registrar_ped = new PedidoBE();
+            //String infPedido;
+
+            try
+            {
+
+                //registrar_ped.Vehiculo.Placa = Convert.ToString (lstVehiculo.SelectedValue);
+                //registrar_ped.Ruta.Nombre_Ruta = lblRuta.Text;
+                ////registrar_ped.Detalle_Ped.Tamano.Tamano = GRIDVIEW; // como asignarle a la base de datos la información del pedido que está en la lista (tam_cant)
+                //registrar_ped.Detalle_Ped.Cantidad = GRIDVIEW;
+
+                //infPedido = servPedido.Registrar_PedidoAsync (registrar_ped);
+
+                //if (infPedido == "Ok")
+                //{
+                //    MessageBox.Show("Sus datos fueron enviados satisfactoriamente");
+                //}
+                //else
+                //{
+                //    MessageBox.Show("Error al intentar registrar el pedido");
+
+                //}
+
+                ContentRegistrar.Visibility = System.Windows.Visibility.Collapsed;
+                ContentBusq.Visibility = System.Windows.Visibility.Visible;
+            }
+            catch (Exception ex)
+            {
+                NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+            }
+            finally
+            {
+                servPedido.CloseAsync();
+                NavigationService.Navigate(new Uri("/Pedidos/frmRegistrarPedido.xaml", UriKind.Relative));
+            }
+ 
+        }
     }
 }
