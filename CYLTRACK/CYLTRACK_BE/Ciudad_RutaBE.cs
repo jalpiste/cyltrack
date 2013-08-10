8efdcb08ee0d69423812ea4aeef43552572ef62a
jackpave0917 <jackpave0917@992608a5-e751-9426-943c-320f8e000771>
2013-08-10 20:54:07 +0000

    modificacion Ciudad_rutaBe

--- CYLTRACK/CYLTRACK_BE/Ciudad_RutaBE.cs
+++ CYLTRACK/CYLTRACK_BE/Ciudad_RutaBE.cs
@@ -29,6 +29,6 @@ namespace Unisangil.CYLTRACK.CYLTRACK_BE
         /// Ciudad_Ruta
         /// </summary>
         [DataMember]
-        public CiudadBE Ciudad { get; set; }
+        public CiudadBE[] Ciudad { get; set; }
     }
 }
