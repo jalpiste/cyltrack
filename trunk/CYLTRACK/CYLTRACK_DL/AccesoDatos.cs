53840a108abf71e2da9671075c054bf1a6510b2c
jackpave0917 <jackpave0917@992608a5-e751-9426-943c-320f8e000771>
2013-10-16 02:47:22 +0000

    modificacion y adicion accesoDatos y PruebaDL

---
+++ trunk/CYLTRACK/CYLTRACK_DL/AccesoDatos.cs
@@ -0,0 +1,216 @@
+﻿/*
+ * Proyecto de grado: Trazabilidad de Cilindros CYLTRACK
+ * Integrantes: Viviana Camacho y Jackelyne Padilla
+ * Director: Fabián Lancheros Currea
+ * Deerechos reservados
+ * */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.Common;
+using System.Configuration;
+
+namespace Unisangil.CYLTRACK.CYLTRACK_DL
+{
+    public class BaseDatos
+    {
+
+        private DbConnection conexion = null;
+        private DbCommand comando = null;
+        private DbTransaction transaccion = null;
+        private string cadenaConexion;
+
+        private static DbProviderFactory factory = null;
+
+        /// <summary>
+        /// Crea una instancia del acceso a la base de datos y configura el acceso a la BD.
+        /// </summary>
+        public BaseDatos()
+        {
+            try
+            {
+                string proveedor = ConfigurationManager.AppSettings.Get("Proveedor");
+                this.cadenaConexion = ConfigurationManager.AppSettings.Get("CadenaConexion");
+                BaseDatos.factory = DbProviderFactories.GetFactory(proveedor);
+            }
+            catch (ConfigurationException ex)
+            {
+                throw;
+            }
+        }
+
+
+        /// <summary>
+        /// Permite desconectarse de la base de datos.
+        /// </summary>
+        public void Desconectar()
+        {
+            if (this.conexion.State.Equals(ConnectionState.Open))
+            {
+                this.conexion.Close();
+            }
+        }
+
+        /// <summary>
+        /// Se concecta con la base de datos.
+        /// </summary>
+        /// <exception cref="BaseDatosException">Si existe un error al conectarse.</exception>
+        public void Conectar()
+        {
+            if (this.conexion != null && !this.conexion.State.Equals(ConnectionState.Closed))
+            {
+                throw new Exception("No existen los parametros de conexión a CYLTRACK BD");
+            }
+            try
+            {
+                if (this.conexion == null)
+                {
+                    this.conexion = factory.CreateConnection();
+                    this.conexion.ConnectionString = cadenaConexion;
+                }
+                this.conexion.Open();
+            }
+            catch (DataException ex)
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Crea un comando en base a una sentencia SQL.
+        /// Ejemplo:
+        /// <code>SELECT * FROM Tabla WHERE campo1=@campo1, campo2=@campo2</code>
+        /// Guarda el comando para el seteo de parámetros y la posterior ejecución.
+        /// </summary>
+        /// <param name="sentenciaSQL">La sentencia SQL con el formato: SENTENCIA [param = @param,]</param>
+        public void CrearComando(string sentenciaSQL)
+        {
+            this.comando = factory.CreateCommand();
+            this.comando.Connection = this.conexion;
+            this.comando.CommandType = CommandType.Text;
+            this.comando.CommandText = sentenciaSQL;
+            if (this.transaccion != null)
+            {
+                this.comando.Transaction = this.transaccion;
+            }
+        }
+
+        /// <summary>
+        /// Asigna un parámetro al comando creado.
+        /// </summary>
+        /// <param name="nombre">El nombre del parámetro.</param>
+        /// <param name="separador">El separador que será agregado al valor del parámetro.</param>
+        /// <param name="valor">El valor del parámetro.</param>
+        public void AsignarParametro(string nombre, string separador, string valor)
+        {
+            int indice = this.comando.CommandText.IndexOf(nombre);
+            string prefijo = this.comando.CommandText.Substring(0, indice);
+            string sufijo = this.comando.CommandText.Substring(indice + nombre.Length);
+            this.comando.CommandText = prefijo + separador + valor + separador + sufijo;
+        }
+
+        /// <summary>
+        /// Ejecuta el comando creado y retorna el resultado de la consulta.
+        /// </summary>
+        /// <returns>El resultado de la consulta.</returns>
+        /// <exception cref="BaseDatosException">Si ocurre un error al ejecutar el comando.</exception>
+        public DbDataReader EjecutarConsulta()
+        {
+            return this.comando.ExecuteReader();
+        }
+
+        /// <summary>
+        /// Ejecuta el comando creado y retorna un escalar.
+        /// </summary>
+        /// <returns>El escalar que es el resultado del comando.</returns>        
+        public int EjecutarEscalar()
+        {
+            int escalar = 0;
+            try
+            {
+                escalar = int.Parse(this.comando.ExecuteScalar().ToString());
+            }
+            catch (InvalidCastException ex)
+            {
+                throw;
+            }
+            return escalar;
+        }
+
+        /// <summary>
+        /// Ejecuta el comando creado.
+        /// </summary>
+        public void EjecutarComando()
+        {
+            this.comando.ExecuteNonQuery();
+        }
+
+        public void CrearComandoSP(string storedProc)
+        {
+            this.comando = factory.CreateCommand();
+            this.comando.Connection = this.conexion;
+            this.comando.CommandType = CommandType.StoredProcedure;
+            this.comando.CommandText = storedProc;
+            if (this.transaccion != null)
+            {
+                this.comando.Transaction = this.transaccion;
+            }
+        }
+
+        public void setParametrosSP(params object[] parametros)
+        {
+            if (parametros.Length % 2 != 0)
+                throw new Exception();
+
+            for (int i = 0; i < parametros.Length; i += 2)
+            {
+                DbParameter param = comando.CreateParameter();
+                param.ParameterName = parametros[i].ToString();
+                param.Value = parametros[i + 1];
+                comando.Parameters.Add(param);
+            }
+        }
+
+
+        /// <summary>
+        /// Comienza una transacción en base a la conexion abierta.
+        /// Todo lo que se ejecute luego de esta ionvocación estará 
+        /// dentro de una tranasacción.
+        /// </summary>
+        public void ComenzarTransaccion()
+        {
+            if (this.transaccion == null)
+            {
+                this.transaccion = this.conexion.BeginTransaction();
+            }
+        }
+
+        /// <summary>
+        /// Cancela la ejecución de una transacción.
+        /// Todo lo ejecutado entre ésta invocación y su 
+        /// correspondiente <c>ComenzarTransaccion</c> será perdido.
+        /// </summary>
+        public void CancelarTransaccion()
+        {
+            if (this.transaccion != null)
+            {
+                this.transaccion.Rollback();
+            }
+        }
+
+        /// <summary>
+        /// Confirma todo los comandos ejecutados entre el <c>ComanzarTransaccion</c>
+        /// y ésta invocación.
+        /// </summary>
+        public void ConfirmarTransaccion()
+        {
+            if (this.transaccion != null)
+            {
+                this.transaccion.Commit();
+            }
+        }
+
+    }
+}
---
+++ trunk/CYLTRACK/CYLTRACK_DL/PruebaDL.cs
@@ -0,0 +1,113 @@
+﻿/*
+ * Proyecto de grado: Trazabilidad de Cilindros CYLTRACK
+ * Integrantes: Viviana Camacho y Jackelyne Padilla
+ * Director: Fabián Lancheros Currea
+ * Deerechos reservados
+ * */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using System.Data.Common;
+using System.Data;
+using Unisangil.CYLTRACK.CYLTRACK_BE;
+
+namespace Unisangil.CYLTRACK.CYLTRACK_DL
+{
+    public class PruebaDL
+    {
+        public List<PruebaBE> ConsultarPruebaBEs()
+        {
+            List<PruebaBE> pruebas = new List<PruebaBE>();
+            try
+            {
+                string nameSP = "spConsultaPrueba";
+                BaseDatos db = new BaseDatos();
+                db.Conectar();
+                db.CrearComandoSP(nameSP);
+
+                DbDataReader datos = db.EjecutarConsulta();
+                PruebaBE p = null;
+                while (datos.Read())
+                {
+                    try
+                    {
+                        p = new PruebaBE();
+                        p.IdPrueba = datos.GetInt32(0);
+                        p.Descripción = datos.GetString(1);
+                        //p.Fecha = datos.GetString(2);
+                        pruebas.Add(p);
+                    }
+                    catch (InvalidCastException ex)
+                    {
+                        throw new Exception("Los tipos no coinciden.", ex);
+                    }
+                    catch (DataException ex)
+                    {
+                        throw new Exception("Error de ADO.NET.", ex);
+                    }
+                }
+                datos.Close();
+                db.Desconectar();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al acceder a la base de datos para obtener los PruebaBEs.");
+            }            
+            return pruebas;
+        }
+
+        public long GuardarPruebaBE(PruebaBE pru)
+        {
+            long codigo = 0;
+            BaseDatos db = new BaseDatos();
+            try
+            {
+                db.Conectar();
+                db.ComenzarTransaccion();                
+                string nameSP = "spCrearPrueba";
+                db.CrearComandoSP(nameSP);
+                db.setParametrosSP("descripcion", pru.Descripción);                
+                codigo = db.EjecutarEscalar();
+                db.ConfirmarTransaccion();
+            }
+            catch (Exception ex)
+            {
+                db.CancelarTransaccion();
+                throw new Exception("Error al crear el PruebaBE.", ex);
+            }
+            
+            finally
+            {
+                db.Desconectar();
+            }
+            return codigo;
+        }
+
+        public void ModificarPruebaBE(PruebaBE prueba)
+        {
+            BaseDatos db = new BaseDatos();
+            try
+            {
+                db.Conectar();
+                db.ComenzarTransaccion();
+                string nameSP = "spModificarPrueba";
+                db.CrearComandoSP(nameSP);
+                db.setParametrosSP("id", prueba.IdPrueba, "descripcion", prueba.Descripción, "fecha", prueba.Fecha);
+                db.EjecutarComando();
+                db.ConfirmarTransaccion();
+            }
+            catch (Exception ex)
+            {
+                db.CancelarTransaccion();                
+                throw new Exception("Error al actualizar el objeto Prueba.", ex);
+            }           
+
+            finally
+            {
+                db.Desconectar();
+            }
+        }
+    }
+}
