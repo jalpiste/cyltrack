e87857659ebb0493ca6fa75ca31e504ce3e4d4be
jackpave0917 <jackpave0917@992608a5-e751-9426-943c-320f8e000771>
2013-10-16 02:03:38 +0000

    adicion PruebaBL

---
+++ trunk/CYLTRACK/CYLTRACK_BL/PruebaBL.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Unisangil.CYLTRACK.CYLTRACK_DL;
+using Unisangil.CYLTRACK.CYLTRACK_BE;
+
+namespace Unisangil.CYLTRACK.CYLTRACK_BL
+{
+    public class PruebaBL
+    {
+        public long CrearPrueba(PruebaBE prueba)
+        {
+            PruebaDL crearPru = new PruebaDL();
+            long resp = 0;
+            try
+            {
+                resp = crearPru.GuardarPruebaBE(prueba);
+            }
+            catch (Exception ex)
+            {
+                //guardar exepcion en el log de bd
+                resp = -1;
+            }
+
+            return resp;
+        }
+    }
+}
