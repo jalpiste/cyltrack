d918db7e0e6ead5e17976860f047e897667788f9
jackpave0917 <jackpave0917@992608a5-e751-9426-943c-320f8e000771>
2013-10-16 02:06:07 +0000

    adicion de PruebaBE

---
+++ CYLTRACK/CYLTRACK_BE/PruebaBE.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Proyecto de grado: Trazabilidad de Cilindros CYLTRACK
+ * Integrantes: Viviana Camacho y Jackelyne Padilla
+ * Director: Fabián Lancheros Currea
+ * Deerechos reservados
+ * */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Unisangil.CYLTRACK.CYLTRACK_BE
+{
+    /// <summary>
+    /// Entidad de negocio usada como prueba
+    /// </summary>
+    public class PruebaBE
+    {
+        int idPrueba;
+        string descripción;
+        DateTime fecha;
+
+        public DateTime Fecha
+        {
+            get { return fecha; }
+            set { fecha = value; }
+        }
+
+        public string Descripción
+        {
+            get { return descripción; }
+            set { descripción = value; }
+        }
+
+        public int IdPrueba
+        {
+            get { return idPrueba; }
+            set { idPrueba = value; }
+        }
+
+
+    }
+}
