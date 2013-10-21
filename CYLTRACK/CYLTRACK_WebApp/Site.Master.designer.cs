64f0edfeb59c2d2f555631c6b9dedc4d588b9607
jackpave0917 <jackpave0917@992608a5-e751-9426-943c-320f8e000771>
2013-10-21 02:26:40 +0000

    modificacion site.master

--- CYLTRACK/CYLTRACK_WebApp/Site.Master.designer.cs
+++ CYLTRACK/CYLTRACK_WebApp/Site.Master.designer.cs
@@ -13,6 +13,15 @@ namespace Unisangil.CYLTRACK.CYLTRACK_WebApp {
     public partial class SiteMaster {
         
         /// <summary>
+        /// Control Head1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlHead Head1;
+        
+        /// <summary>
         /// Control HeadContent.
         /// </summary>
         /// <remarks>
