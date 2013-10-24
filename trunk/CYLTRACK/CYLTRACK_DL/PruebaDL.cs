a391697472dbad3c0348e7bc8b09db2329f737f0
cesarcurrea <cesarcurrea@992608a5-e751-9426-943c-320f8e000771>
2013-10-24 02:33:52 +0000

    Ajuste AppTest

--- trunk/CYLTRACK/CYLTRACK_DL/PruebaDL.cs
+++ trunk/CYLTRACK/CYLTRACK_DL/PruebaDL.cs
@@ -17,16 +17,36 @@ namespace Unisangil.CYLTRACK.CYLTRACK_DL
 {
     public class PruebaDL
     {
-        public List<PruebaBE> ConsultarPruebaBEs()
+        public List<PruebaBE> ConsultarPruebas(int idPrueba)
         {
             List<PruebaBE> pruebas = new List<PruebaBE>();
             try
             {
-                string nameSP = "spConsultaPrueba";
+                string nameSP = "ConsultarPruebas";
                 BaseDatos db = new BaseDatos();
                 db.Conectar();
                 db.CrearComandoSP(nameSP);
+                DbParameter[] parametros = new DbParameter[3];
+                parametros[0] = db.Comando.CreateParameter();
+                parametros[0].ParameterName = "vrIdPrueba";
+                parametros[0].Value = idPrueba;
+                parametros[0].Direction = ParameterDirection.Input;
+                db.Comando.Parameters.Add(parametros[0]);
 
+                parametros[1] = db.Comando.CreateParameter();
+                parametros[1].ParameterName = "vrCodResult";
+                parametros[1].Value = 0;
+                parametros[1].Direction = ParameterDirection.Output;
+                db.Comando.Parameters.Add(parametros[1]);
+
+                parametros[2] = db.Comando.CreateParameter();
+                parametros[2].ParameterName = "vrDescResult";
+                parametros[2].Value = "";
+                parametros[2].Direction = ParameterDirection.Output;
+                parametros[2].Size = 200;
+                parametros[2].DbType = DbType.String;
+                db.Comando.Parameters.Add(parametros[2]);
+                
                 DbDataReader datos = db.EjecutarConsulta();
                 PruebaBE p = null;
                 while (datos.Read())
@@ -34,9 +54,9 @@ namespace Unisangil.CYLTRACK.CYLTRACK_DL
                     try
                     {
                         p = new PruebaBE();
-                        p.IdPrueba = datos.GetInt32(0);
+                        p.IdPrueba = Int32.Parse(datos.GetValue(0).ToString());
                         p.Descripción = datos.GetString(1);
-                        //p.Fecha = datos.GetString(2);
+                        p.Fecha = datos.GetDateTime(2);
                         pruebas.Add(p);
                     }
                     catch (InvalidCastException ex)
@@ -68,7 +88,6 @@ namespace Unisangil.CYLTRACK.CYLTRACK_DL
                 db.ComenzarTransaccion();
                 string nameSP = "CrearRegistro";
                 string descripcion = "";
-                int cod=90;
                 db.CrearComandoSP(nameSP);
 
                 DbParameter[] parametros = new DbParameter[3];
@@ -92,11 +111,11 @@ namespace Unisangil.CYLTRACK.CYLTRACK_DL
                 parametros[2].Size = 200;
                 parametros[2].DbType = DbType.String;
                 db.Comando.Parameters.Add(parametros[2]);
-                               
+
                 db.EjecutarComando();
                 codigo = long.Parse(db.Comando.Parameters[1].Value.ToString());
                 descripcion = db.Comando.Parameters[2].Value.ToString();
-                db.ConfirmarTransaccion();               
+                db.ConfirmarTransaccion();
             }
             catch (Exception ex)
             {
@@ -111,21 +130,57 @@ namespace Unisangil.CYLTRACK.CYLTRACK_DL
             return codigo;
         }
 
-        public void ModificarPruebaBE(PruebaBE prueba)
+        public int ModificarPrueba(PruebaBE prueba)
         {
+            int resp = -1;
+            string descripcion = "";
             BaseDatos db = new BaseDatos();
             try
             {
                 db.Conectar();
                 db.ComenzarTransaccion();
-                string nameSP = "spModificarPrueba";
+                string nameSP = "ModificarPrueba";
                 db.CrearComandoSP(nameSP);
-                db.setParametrosSP("id", prueba.IdPrueba, "descripcion", prueba.Descripción, "fecha", prueba.Fecha);
+                //db.setParametrosSP("id", prueba.IdPrueba, "descripcion", prueba.Descripción, "fecha", prueba.Fecha);
+
+                DbParameter[] parametros = new DbParameter[4];
+                parametros[0] = db.Comando.CreateParameter();
+                parametros[0].ParameterName = "vrDescripcion";
+                parametros[0].Value = prueba.Descripción;
+                parametros[0].Direction = ParameterDirection.Input;
+                parametros[0].Size = 50;
+                db.Comando.Parameters.Add(parametros[0]);
+
+                parametros[1] = db.Comando.CreateParameter();
+                parametros[1].ParameterName = "vrIdPrueba";
+                parametros[1].Value = prueba.IdPrueba;
+                parametros[1].Direction = ParameterDirection.Input;
+                parametros[1].Size = 50;
+                db.Comando.Parameters.Add(parametros[1]);
+
+                parametros[2] = db.Comando.CreateParameter();
+                parametros[2].ParameterName = "vrCodResult";
+                parametros[2].Value = 0;
+                parametros[2].Direction = ParameterDirection.Output;
+                db.Comando.Parameters.Add(parametros[2]);
+
+                parametros[3] = db.Comando.CreateParameter();
+                parametros[3].ParameterName = "vrDescResult";
+                parametros[3].Value = "";
+                parametros[3].Direction = ParameterDirection.Output;
+                parametros[3].Size = 200;
+                parametros[3].DbType = DbType.String;
+                db.Comando.Parameters.Add(parametros[3]);
+
+                db.EjecutarComando();
+                resp = int.Parse(db.Comando.Parameters[2].Value.ToString());
+                descripcion = db.Comando.Parameters[3].Value.ToString();
                 db.EjecutarComando();
                 db.ConfirmarTransaccion();
             }
             catch (Exception ex)
             {
+                resp = -1;
                 db.CancelarTransaccion();
                 throw new Exception("Error al actualizar el objeto Prueba.", ex);
             }
@@ -134,6 +189,7 @@ namespace Unisangil.CYLTRACK.CYLTRACK_DL
             {
                 db.Desconectar();
             }
+            return resp;
         }
     }
 }
