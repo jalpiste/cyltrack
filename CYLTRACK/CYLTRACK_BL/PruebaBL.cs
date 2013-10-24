2615b30c7bcb172425c87757677b019b9707c36b
cesarcurrea <cesarcurrea@992608a5-e751-9426-943c-320f8e000771>
2013-10-24 02:33:37 +0000

    Ajuste AppTest

--- CYLTRACK/CYLTRACK_BL/PruebaBL.cs
+++ CYLTRACK/CYLTRACK_BL/PruebaBL.cs
@@ -11,11 +11,43 @@ namespace Unisangil.CYLTRACK.CYLTRACK_BL
     {
         public long CrearPrueba(PruebaBE prueba)
         {
-            PruebaDL crearPru = new PruebaDL();
+            PruebaDL pru = new PruebaDL();
             long resp = 0;
             try
             {
-                resp = crearPru.GuardarPruebaBE(prueba);
+                resp = pru.GuardarPruebaBE(prueba);
+            }
+            catch (Exception ex)
+            {
+                //guardar exepcion en el log de bd
+                resp = -1;
+            }
+
+            return resp;
+        }
+
+        public List<PruebaBE> ConsultarPruebas(int idPrueba)
+        {
+            PruebaDL pru = new PruebaDL();
+            List<PruebaBE> pruebas = new List<PruebaBE>();
+            try
+            {
+                pruebas = pru.ConsultarPruebas(idPrueba);
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return pruebas;
+        }
+
+        public int ModificarPrueba(PruebaBE prueba)
+        {
+            PruebaDL pru = new PruebaDL();
+            int resp = 0;
+            try
+            {
+                resp = pru.ModificarPrueba(prueba);
             }
             catch (Exception ex)
             {
