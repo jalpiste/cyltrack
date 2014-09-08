57177df7c3da00d1aa76bdfd5d326538862b6a91
jackpave0917 <jackpave0917@992608a5-e751-9426-943c-320f8e000771>
2014-09-08 06:07:55 +0000

    Reporte de Hallazgos 4, CU0011 Modificación de Pedidos

--- trunk/CYLTRACK/CYLTRACK_WebApp/Pedido/frmModificarPedido.aspx.designer.cs
+++ trunk/CYLTRACK/CYLTRACK_WebApp/Pedido/frmModificarPedido.aspx.designer.cs
@@ -238,6 +238,24 @@ namespace Unisangil.CYLTRACK.CYLTRACK_WebApp.Pedido {
         protected global::System.Web.UI.WebControls.TextBox txtCantidad;
         
         /// <summary>
+        /// Control RequiredFieldValidator1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator1;
+        
+        /// <summary>
+        /// Control RegularExpressionValidator1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RegularExpressionValidator RegularExpressionValidator1;
+        
+        /// <summary>
         /// Control btnEjecutar.
         /// </summary>
         /// <remarks>
