9c9696420b13b947d698612b9c090bff99da8ae7
vivis1225 <vivis1225@992608a5-e751-9426-943c-320f8e000771>
2014-11-05 06:33:52 +0000

    modificación registrar venta y consultar cilindro y venta

--- trunk/CYLTRACK/CYLTRACK_PHONE/Ventas/frmConsultaVenta.xaml.cs
+++ trunk/CYLTRACK/CYLTRACK_PHONE/Ventas/frmConsultaVenta.xaml.cs
@@ -10,6 +10,10 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using CYLTRACK_PHONE.ClienteService;
+using CYLTRACK_PHONE.VentaService;
+using CYLTRACK_PHONE.VehiculoService;
+
 
 namespace Unisangil.CYLTRACK.CYLTRACK_PHONE.Ventas
 {
@@ -21,8 +25,58 @@ namespace Unisangil.CYLTRACK.CYLTRACK_PHONE.Ventas
         }
         private void btnConsultar_Click(object sender, RoutedEventArgs e)
         {
-            ContentBusq.Visibility = System.Windows.Visibility.Collapsed;
-            ContentCliente.Visibility = System.Windows.Visibility.Visible;
+            
+            ClienteServiceClient servCliente = new ClienteServiceClient();
+           
+            servCliente.Consultar_ClienteAsync(txtCedula.Text);
+            servCliente.Consultar_ClienteCompleted += new EventHandler<Consultar_ClienteCompletedEventArgs>(PoblarCliente);
+
+            VentaServiceClient servVenta = new VentaServiceClient();
+
+            servVenta.ConsultarVentaAsync(lblCodVenta.Text);
+            servVenta.ConsultarVentaCompleted += new EventHandler<ConsultarVentaCompletedEventArgs>(PoblarInfVenta);
+
+        }
+        
+        private void PoblarCliente(object sender, Consultar_ClienteCompletedEventArgs e)
+        {
+            
+            try
+            {
+                if (e.Result.Cedula == null)
+                {
+                    MessageBox.Show("El cliente no se encuentra registrado");
+                    NavigationService.Navigate(new Uri("/Ventas/frmConsultaVenta.xaml", UriKind.Relative));
+                }
+                else
+                {
+                    ContentBusq.Visibility = System.Windows.Visibility.Collapsed;
+                    ContentCliente.Visibility = System.Windows.Visibility.Visible;
+
+                    
+                    lblCedulaCli.Text = e.Result.Cedula;
+                    txtNombres.Text = e.Result.Nombres_Cliente;
+                    txtPrApellido.Text = e.Result.Apellido_1;
+                    txtSgApellido.Text = e.Result.Apellido_2;
+                    lblDirecciones.Text = "";
+
+                    //foreach  (UbicacionBE ubi in e.Result.ListaDirecciones)
+                    //{
+                    //    lblDirecciones.Text += ubi.Id_Ubicacion + "-" + ubi.Ciudad.Nombre_Ciudad + "-" + ubi.Barrio + "-" + (ubi.Direccion.Length > 20 ? ubi.Direccion.Substring(0, 20) : ubi.Direccion) + "\n";
+                    //}
+
+                }
+                txtCedula.Text = "";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ha ocurrido un error en el sistema");
+                NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+            }
+            finally
+            {
+                btnContinuar.Focus();
+            }
         }
 
         private void btnNvConsulta_MouseEnter(object sender, MouseEventArgs e)
@@ -38,15 +92,54 @@ namespace Unisangil.CYLTRACK.CYLTRACK_PHONE.Ventas
 
         private void btnContinuar_Click(object sender, RoutedEventArgs e)
         {
-            ContentCliente.Visibility = System.Windows.Visibility.Collapsed;
-            ContentCilindro.Visibility = System.Windows.Visibility.Visible;
+            VehiculoServiceClient servVehiculo = new VehiculoServiceClient();
+
+            servVehiculo.Consultar_ConductorAsync(lblCodVent.Text);
+            servVehiculo.Consultar_ConductorCompleted += new EventHandler<Consultar_ConductorCompletedEventArgs>(PoblarVendedor);
+        }
+
+        private void PoblarVendedor(object sender, Consultar_ConductorCompletedEventArgs e)
+        {
+            txtNombsVendedor.Text = e.Result.Nombres_Conductor + " " + e.Result.Apellido_1 + " " + e.Result.Apellido_2;
+        }
+
+        private void PoblarInfVenta(object sender, ConsultarVentaCompletedEventArgs e)
+        {
+
+            try
+            {
+                if (e.Result.Id_Venta == null)
+                {
+                    MessageBox.Show("El cliente no tiene asignado ninguna venta reciente");
+                    NavigationService.Navigate(new Uri("/Ventas/frmConsultaVenta.xaml", UriKind.Relative));
+                }
+                else
+                {
+                    lblCodVenta.Text = e.Result.Id_Venta;
+                    lblFechaVenta.Text = Convert.ToString(e.Result.Fecha);
+                    lblHoraVenta.Text = Convert.ToString(e.Result.Fecha.TimeOfDay);
+
+                    foreach (Detalle_VentaBE info in e.Result.Lista_Detalle_Venta)
+                    {
+                        lblCilindroVenta.Text += info.Id_Cilindro_Salida + "-" + info.Tamano + "-" + info.Tipo_Cilindro;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ha ocurrido un error en el sistema");
+                NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+            }
+            finally
+            {
+                btnNvConsulta.Focus();
+            }
         }
 
         private void btnAtras_Click(object sender, RoutedEventArgs e)
         {
             ContentCliente.Visibility = System.Windows.Visibility.Collapsed;
             ContentBusq.Visibility = System.Windows.Visibility.Visible;
-
         }
 
         private void btnNvConsulta_Click(object sender, RoutedEventArgs e)
@@ -55,14 +148,10 @@ namespace Unisangil.CYLTRACK.CYLTRACK_PHONE.Ventas
             ContentBusq.Visibility = System.Windows.Visibility.Visible;
         }
 
-
-
         private void btnMenuPrincipal_Click(object sender, RoutedEventArgs e)
         {
             NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
         }
-
-
 
     }
 }
