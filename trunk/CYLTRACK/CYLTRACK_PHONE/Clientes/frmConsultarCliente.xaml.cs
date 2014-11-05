75784491f45af6a3c4b8d6fa856f092116ce4ea5
vivis1225 <vivis1225@992608a5-e751-9426-943c-320f8e000771>
2014-11-05 06:35:09 +0000

    modificación interfaces consultar y registrar cliente

--- trunk/CYLTRACK/CYLTRACK_PHONE/Clientes/frmConsultarCliente.xaml.cs
+++ trunk/CYLTRACK/CYLTRACK_PHONE/Clientes/frmConsultarCliente.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
 using CYLTRACK_PHONE.ClienteService;
+
+
 namespace Unisangil.CYLTRACK.Cyltrack_phone.Clientes
 {
     public partial class frmConsultarCliente : PhoneApplicationPage
@@ -20,30 +22,159 @@ namespace Unisangil.CYLTRACK.Cyltrack_phone.Clientes
             InitializeComponent();
         }
 
-        private void btnRegistrar_Click(object sender, RoutedEventArgs e)
-        {            
+        private void btnConsultar_Click(object sender, RoutedEventArgs e)
+        {
             ClienteServiceClient servCliente = new ClienteServiceClient();
-            servCliente.ConsultarExistenciasClientesAsync(txtCedula.Text);
-            servCliente.ConsultarExistenciasClientesCompleted+=new EventHandler<ConsultarExistenciasClientesCompletedEventArgs>(PoblarCliente);
+            servCliente.Consultar_ClienteAsync(txtCedula.Text);
+            servCliente.Consultar_ClienteCompleted += new EventHandler<Consultar_ClienteCompletedEventArgs>(PoblarCliente);
+            servCliente.CloseAsync();
         }
 
-        private void PoblarCliente(object sender, ConsultarExistenciasClientesCompletedEventArgs e)
+        private void PoblarCliente(object sender, Consultar_ClienteCompletedEventArgs e)
         {
-            if (e.Result > 0)
+           
+
+            try
             {
-                ContentBusq.Visibility = System.Windows.Visibility.Collapsed;
-                ContentDatosP.Visibility = System.Windows.Visibility.Visible;
+                if (e.Result.Cedula == null)
+                {
+                    MessageBox.Show("El cliente no se encuentra registrado");
+                    NavigationService.Navigate(new Uri("/Clientes/frmConsultarCliente.xaml", UriKind.Relative));
+                }
+                else
+                {
+                    ContentBusq.Visibility = System.Windows.Visibility.Collapsed;
+                    ContentDatosP.Visibility = System.Windows.Visibility.Visible;
+
+                    lblCedulaCli.Text = e.Result.Cedula;
+                    txtNombres.Text = e.Result.Nombres_Cliente;
+                    txtPrApellido.Text = e.Result.Apellido_1;
+                    txtSgApellido.Text = e.Result.Apellido_2;
+                    lblDirecciones.Text = "";
+                   
+                    foreach (UbicacionBE ubi in e.Result.ListaDirecciones)
+                    {
+                        lblDirecciones.Text += ubi.Id_Ubicacion + "--"+ ubi.Ciudad.Nombre_Ciudad + "--" + ubi.Barrio + "--"+ (ubi.Direccion.Length > 20 ? ubi.Direccion.Substring(0,20): ubi.Direccion) + "--" + ubi.Telefono_1 +"\n";
+                    }
+   
+                }
+                txtCedula.Text = "";
             }
-            else
-                MessageBox.Show("Cliente no existe");
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ha ocurrido un error en el sistema");
+                NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+            }
+            finally
+            {
+                NavigationService.Navigate(new Uri("/Clientes/frmConsultarCliente.xaml", UriKind.Relative));
+
+            }
+        }
+
+        private void hplAgregarNuevaUbi_Click(object sender, RoutedEventArgs e)
+        {
+
+            ContentModificarCliente2.Visibility = System.Windows.Visibility.Collapsed;
+            ContentAgregarUbicacion.Visibility = System.Windows.Visibility.Visible;
+
+            ClienteServiceClient servCliente = new ClienteServiceClient();
+
+            ClienteBE nuevadir = new ClienteBE();
+                        
+            UbicacionBE ubicacion = new UbicacionBE();
+            ubicacion.Direccion = txtNuevaDir.Text;
+            ubicacion.Barrio = txtNuevoBarrio.Text;
+            ubicacion.Telefono_1 = txtTelefono.Text;
+            CiudadBE ciudad = new CiudadBE();
+            ciudad.Id_Ciudad = "1";
+            ubicacion.Ciudad = ciudad;
+            nuevadir.Ubicacion = ubicacion;
+
+
+            servCliente.Agregar_UbicacionAsync(nuevadir);
+            servCliente.Agregar_UbicacionCompleted += new EventHandler<Agregar_UbicacionCompletedEventArgs>(AgregarUbiNueva);
+            servCliente.CloseAsync();
+
+            
+
+        }
+
+        private void AgregarUbiNueva(object sender, Agregar_UbicacionCompletedEventArgs e)
+        {
+            
+            ContentModificarCliente2.Visibility = System.Windows.Visibility.Visible;
+            ContentAgregarUbicacion.Visibility = System.Windows.Visibility.Collapsed;
+            
+            try
+            {
+                if (e.Result < 0)
+                {
+                    MessageBox.Show("Error al registrar la nueva dirección, Por favor intente de nuevo");
+
+                }
+                else
+                    MessageBox.Show("La dirección fue registrada satisfactoriamente");
+            }
+
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ha ocurrido un error en el sistema, intente nuevamente.");
+                NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+            }
+            finally
+            {
+                NavigationService.Navigate(new Uri("/Clientes/frmConsultarCliente.xaml", UriKind.Relative));
+            }
 
         }
        
         private void hplModificarCliente_Click(object sender, RoutedEventArgs e)
         {
-            ContentDatosP.Visibility = System.Windows.Visibility.Collapsed;
-            ContentModificarCliente.Visibility = System.Windows.Visibility.Visible;
-            PageTitle.Text = "MODIFICAR CLIENTE";
+            
+            ClienteServiceClient servCliente = new ClienteServiceClient();
+            servCliente.Consultar_ClienteAsync(lblCedulaCli.Text);
+            servCliente.Consultar_ClienteCompleted += new EventHandler<Consultar_ClienteCompletedEventArgs>(ModificarCli);
+            servCliente.CloseAsync();
+        }
+
+        private void ModificarCli(object sender, Consultar_ClienteCompletedEventArgs e)
+        {
+           
+            try
+            {
+                if (e.Result.Cedula == null)
+                {
+                    MessageBox.Show("El cliente no se encuentra registrado");
+                    NavigationService.Navigate(new Uri("/Clientes/frmConsultarCliente.xaml", UriKind.Relative));
+                }
+
+                else
+                {
+                    ContentDatosP.Visibility = System.Windows.Visibility.Collapsed;
+                    ContentModificarCliente2.Visibility = System.Windows.Visibility.Visible;
+                    PageTitle.Text = "MODIFICAR CLIENTE";
+
+
+                    lblCedulaCli2.Text =lblCedulaCli.Text;
+                    txtNombres2.Text = e.Result.Nombres_Cliente;
+                    txtPrApellido2.Text = e.Result.Apellido_1;
+                    txtSgApellido2.Text = e.Result.Apellido_2;
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ha ocurrido un error en el sistema");
+                NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+            }
+            finally
+            {
+               NavigationService.Navigate(new Uri("/Clientes/frmConsultarCliente.xaml", UriKind.Relative));
+
+            }
+         
         }
 
         private void btnMenu_Click(object sender, RoutedEventArgs e)
@@ -52,35 +183,106 @@ namespace Unisangil.CYLTRACK.Cyltrack_phone.Clientes
         }
 
 
-        private void hplNuevaUbi_Click(object sender, RoutedEventArgs e)
-        {
-            ContentModificarCliente.Visibility = System.Windows.Visibility.Collapsed;
-            ContentAgregarUbicacion.Visibility = System.Windows.Visibility.Visible;
-        }
-
         private void btnMenuConsul_Click(object sender, RoutedEventArgs e)
         {
-            NavigationService.GoBack();
+            ContentModificarCliente2.Visibility = System.Windows.Visibility.Collapsed;
+            ContentDatosP.Visibility = System.Windows.Visibility.Visible;
         }
 
         private void btnGuardarModif_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Sus datos fueron enviados satisfactoriamente");
-            ContentModificarCliente.Visibility = System.Windows.Visibility.Collapsed;
+           
+            ClienteServiceClient servCliente = new ClienteServiceClient();
+
+            ClienteBE cliente = new ClienteBE();
+
+
+            cliente.Nombres_Cliente = txtNombres2.Text;
+            cliente.Apellido_1 = txtPrApellido2.Text;
+            cliente.Apellido_2 = txtSgApellido2.Text;
+
+            UbicacionBE ubi = new UbicacionBE();
+
+            if (Convert.ToBoolean(lblDireccion.Visibility = System.Windows.Visibility.Visible))
+            {
+
+                ubi.Direccion = txtDir.Text;
+                ubi.Barrio = txtBarrio.Text;
+                ubi.Telefono_1 = txtTel.Text;
+                ubi.Ciudad.Id_Ciudad = "1";
+                ubi.Ciudad.Departamento.Id_Departamento = "1";
+
+            }
+
+            servCliente.ModificarNombreClienteAsync(cliente);
+            servCliente.ModificarNombreClienteCompleted += new EventHandler<ModificarNombreClienteCompletedEventArgs>(ModificarCliente);
+            servCliente.ModificarDirClienteAsync(ubi);
+            servCliente.ModificarDirClienteCompleted += new EventHandler<ModificarDirClienteCompletedEventArgs>(ModificarUbicacion);
+            servCliente.CloseAsync();
+                        
+        }
+        private void ModificarUbicacion(object sender, ModificarDirClienteCompletedEventArgs e)
+        {
+            MessageBox.Show("El cliente fue modificado satisfactoriamente");
+            ContentModificarCliente2.Visibility = System.Windows.Visibility.Collapsed;
             ContentBusq.Visibility = System.Windows.Visibility.Visible;
+            try
+            {
+                if (e.Result < 0)
+                {
+                    MessageBox.Show("Error al modificar dirección del cliente, Por favor intente de nuevo");
+                }
+                else
+                    MessageBox.Show("La dirección fue modificada satisfactoriamente");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ha ocurrido un error en el sistema, intente nuevamente.");
+                NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+            }
+            finally
+            {
+                NavigationService.Navigate(new Uri("/Clientes/frmConsultarCliente.xaml", UriKind.Relative));
+            }
+        }
+        private void ModificarCliente(object sender, ModificarNombreClienteCompletedEventArgs e)
+        {
+            MessageBox.Show("El cliente fue modificado satisfactoriamente");
+            ContentModificarCliente2.Visibility = System.Windows.Visibility.Collapsed;
+            ContentBusq.Visibility = System.Windows.Visibility.Visible;
+            try
+            {
+                if (e.Result < 0)
+                {
+                    MessageBox.Show("Error al crear el cliente, Por favor intente de nuevo");
+
+                }
+                else
+                    MessageBox.Show("El cliente fue registrado satisfactoriamente. Código: " + e.Result);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ha ocurrido un error en el sistema, intente nuevamente.");
+                NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+            }
+            finally
+            {
+                NavigationService.Navigate(new Uri("/Clientes/frmRegistrarCliente.xaml", UriKind.Relative));
+            }
+
         }
 
         private void btnAtras_Click(object sender, RoutedEventArgs e)
         {
             ContentAgregarUbicacion.Visibility = System.Windows.Visibility.Collapsed;
-            ContentModificarCliente.Visibility = System.Windows.Visibility.Visible;
+            ContentModificarCliente2.Visibility = System.Windows.Visibility.Visible;
         }
 
         private void btnGuardar_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("Sus datos fueron enviados satisfactoriamente");
             ContentAgregarUbicacion.Visibility = System.Windows.Visibility.Collapsed;
-            ContentModificarCliente.Visibility = System.Windows.Visibility.Visible;
+            ContentModificarCliente2.Visibility = System.Windows.Visibility.Visible;
         }
 
         private void btnNuevaCons_Click(object sender, RoutedEventArgs e)
@@ -88,5 +290,64 @@ namespace Unisangil.CYLTRACK.Cyltrack_phone.Clientes
             ContentDatosP.Visibility = System.Windows.Visibility.Collapsed;
             ContentBusq.Visibility = System.Windows.Visibility.Visible;
         }
+
+        private void btnBuscar_Click(object sender, RoutedEventArgs e)
+        {
+            ClienteServiceClient servCliente = new ClienteServiceClient();
+            servCliente.ConsultarDirCliPorUbicaAsync(txtIdDir.Text);
+            servCliente.ConsultarDirCliPorUbicaCompleted += new EventHandler<ConsultarDirCliPorUbicaCompletedEventArgs>(ModificarDireccion);
+            servCliente.CloseAsync();
+        }
+
+        private void ModificarDireccion(object sender, ConsultarDirCliPorUbicaCompletedEventArgs e)
+        {
+
+            try
+            {
+                if (e.Result.Id_Ubicacion == null)
+                {
+                    MessageBox.Show("La dirección no se encuentra registrada, intente de nuevo");
+                }
+                else
+                {
+                    txtDir.Text = e.Result.Direccion;
+                    txtBarrio.Text = e.Result.Barrio;
+                    txtTel.Text = e.Result.Telefono_1;
+
+                    lblDireccion.Visibility = System.Windows.Visibility.Visible;
+                    txtDir.Visibility = System.Windows.Visibility.Visible;
+                    lblBarrio.Visibility = System.Windows.Visibility.Visible;
+                    txtBarrio.Visibility = System.Windows.Visibility.Visible;
+                    lblDep.Visibility = System.Windows.Visibility.Visible;
+                    lblDepNom.Visibility = System.Windows.Visibility.Visible;
+                    lblCiudad.Visibility = System.Windows.Visibility.Visible;
+                    lblCiudadNombre.Visibility = System.Windows.Visibility.Visible;
+                    lblTel.Visibility = System.Windows.Visibility.Visible;
+                    txtTel.Visibility = System.Windows.Visibility.Visible;
+                }
+
+                txtIdDir.Text = "";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ha ocurrido un error en el sistema");
+                NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+            }
+            finally
+            {
+                NavigationService.Navigate(new Uri("/Clientes/frmConsultarCliente.xaml", UriKind.Relative));
+
+            }
+        }
+
+        
+
+        
+
+        
+
+
+
+
     }
 }
