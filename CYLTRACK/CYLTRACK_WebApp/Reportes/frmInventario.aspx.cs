08bfcd9e8175599003e301e330a09de16aecd504
jackpave0917 <jackpave0917@992608a5-e751-9426-943c-320f8e000771>
2014-11-28 01:53:24 +0000

    modificacion inventario web

--- CYLTRACK/CYLTRACK_WebApp/Reportes/frmInventario.aspx.cs
+++ CYLTRACK/CYLTRACK_WebApp/Reportes/frmInventario.aspx.cs
@@ -11,12 +11,18 @@ using CYLTRACK_WebApp.ReporteService;
 using CYLTRACK_WebApp.VehiculoService;
 using System.Data;
 using System.Windows.Forms;
+using iTextSharp.text.pdf;
+using System.IO;
+using System.Web.UI.WebControls.WebParts;
+
 
 namespace Unisangil.CYLTRACK.CYLTRACK_WebApp
 {
     public partial class Inventario : System.Web.UI.Page
     {
         List<CilindroBE> lstDetail = new List<CilindroBE>();
+        List<CilindroBE> lsAux = new List<CilindroBE>();
+        List<CilindroBE> lsAuxDos = new List<CilindroBE>();
         DataTable objdtLista;
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -94,12 +100,13 @@ namespace Unisangil.CYLTRACK.CYLTRACK_WebApp
             try 
             {
                 reporte.Tipo_Cilindro = "'" + lstTipoCil.SelectedItem.Text + "'";
-                reporte.IdUbicacion = "'" + lstUbicacion.SelectedItem.Text + "'";
+                reporte.IdUbicacion = "'" + lstUbicacion.SelectedValue + "'";
                 reporte.IdVehiculo = "'" + lstPlacaVehículo.SelectedValue + "'";
                 reporte.Fecha_Inicial = Convert.ToDateTime(txtFecha.Text);
 
                 List<Ubicacion_CilindroBE> datosConsulta = new List<Ubicacion_CilindroBE>(servReporte.ConsultarCilInventario(reporte));
 
+           // lsAux = (List<TamanoBE>)Session["lista"];
             table.Columns.Add("Ubicacion");
             table.Columns.Add("CodigosCil");
             table.Columns.Add("Tamano");
@@ -131,6 +138,7 @@ namespace Unisangil.CYLTRACK.CYLTRACK_WebApp
             gvInventario.DataBind();
             gvCantidad.DataSource = tabla2;
             gvCantidad.DataBind();
+            //Session["lsAuxDos"] = table;
             divInventario.Visible = true;
             divBotones.Visible = true;
 
@@ -172,6 +180,45 @@ namespace Unisangil.CYLTRACK.CYLTRACK_WebApp
             {
                 serVehiculo.Close();
             }  
+        }
+
+        protected void btnImp_Click(object sender, EventArgs e)
+        {
+            //Document document = new Document(); 
+            //PdfWriter.GetInstance(document, new FileStream("devjoker.pdf", FileMode.OpenOrCreate)); 
+            //document.Open(); 
+            //document.Add(new Paragraph("Este es mi primer PDF al vuelo")); 
+            //document.Close();
+
+            Response.Clear();
+            Response.ContentType = "application/pdf";
+            Response.AddHeader("content-disposition", "attachment;filename=Formulario.pdf");
+            FillPDF(Server.MapPath("Plantilla.pdf"), Response.OutputStream);
+            
+        }
+
+        public void FillPDF(string template, Stream stream)
+        {
+            
+            PdfReader reader = new PdfReader(template);
+            PdfStamper stamp = new PdfStamper(reader, stream);
+
+            stamp.AcroFields.SetField("FECHADEREPORTE", txtFecha.Text);
+            stamp.AcroFields.SetField("TIPODECILINDRO", lstTipoCil.SelectedItem.Text);
+            stamp.AcroFields.SetField("UBICACIÓN", lstTipoCil.SelectedItem.Text);
+            stamp.AcroFields.SetField("ITEM", lstPlacaVehículo.SelectedItem.Text);
+
+            //foreach(Datatable )
+            //{
+            
+            //}
+
+            //stamp.AcroFields.SetField("UBICIL1", lstTipoCil.SelectedItem.Text);
+            //stamp.AcroFields.SetField("CAMPO2", lstUbicacion.SelectedItem.Text);
+            //stamp.AcroFields.SetField("CAMPO1", lstTipoCil.SelectedItem.Text);
+            //stamp.AcroFields.SetField("CAMPO2", lstUbicacion.SelectedItem.Text);
+            stamp.FormFlattening = true;
+            stamp.Close();
         }
      }
 }
